da1f8ac8d00f34165b460ce0ef28116267228e50
Cayman Heng <hengcayman@gmail.com>
2020-10-18 10:44:53 -0400

    added user prompts for password criteria

--- script.js
+++ script.js
@@ -14,31 +14,6 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-// Define ASCII Character Code Array
-
-var lowerCaseCharCode = charCodeArray(65, 90);
-var upperCaseCharCode = charCodeArray(97, 122);
-var numericCharCode = charCodeArray(48, 57);
-var specialCharCode = charCodeArray(33, 47).concat(
-  charCodeArray(58,64)
-).concat(
-  charCodeArray(91,96)
-).concat(
-  charCodeArray(123, 126)
-);
-
-
-// Create a function to loop through Character Code Array
-
-function charCodeArray(x, y) {
-  var array = []
-  for (var i = x; i <= y; i++) {
-    array.push(i)
-  }
-  return array
-}
-
 // User prompts for password criteria. Create logic to only show next criteria if user inputs a valid option
 
 var passwordLengthPrompt = prompt("Please choose a password length of at least 8 characters and no more than 128 characters.");
@@ -57,10 +32,4 @@ numericPrompt = numericPrompt.toLowerCase();
 
 var specialPrompt = prompt("Do you want the password to include special character\(s)\? Answer 'yes' or 'no.'");
 specialPrompt = specialPrompt.toLowerCase();
-
-// Function to generate password
-
-function generatePassword() {
-  
-}
 
