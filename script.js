f23d939b0d6da745666ed41e262ae5ffdfd45191
Cayman Heng <hengcayman@gmail.com>
2020-10-18 16:06:23 -0400

    added commentary to script file to improve ease of understanding

--- script.js
+++ script.js
@@ -19,10 +19,29 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
+
+  //User prompts for password criteria. Create logic to only show next criteria if user inputs a valid option
+
+  var lowerCasePrompt = prompt("Do you want the password to include lowercase character\(s)\? Answer 'yes' or 'no.'");
+  lowerCasePrompt = lowerCasePrompt.toLowerCase();
+  
+  var upperCasePrompt = prompt("Do you want the password to include uppercase character\(s)\? Answer 'yes' or 'no.'");
+  upperCasePrompt = upperCasePrompt.toLowerCase();
+  
+  var numericPrompt = prompt("Do you want the password to include numeric character\(s)\? Answer 'yes' or 'no.'");
+  numericPrompt = numericPrompt.toLowerCase();
+  
+  var specialPrompt = prompt("Do you want the password to include special character\(s)\? Answer 'yes' or 'no.'");
+  specialPrompt = specialPrompt.toLowerCase();
+  
+  var passwordLengthPrompt = prompt("Please choose a password length of at least 8 characters and no more than 128 characters.");
+
+  // Validate user selected criteria. If no character type is selected or if character length is not between 8 and 128, user will be asked to start over.
+
   if (lowerCasePrompt === "no" && upperCasePrompt === "no" && numericPrompt === "no") {
-    alert("Please choose at least one character type. Refresh the page and start over again.")
+    alert("Please choose at least one character type. Click the 'Generate Password' button to start over again.")
   } else if (passwordLengthPrompt < 8 || passwordLengthPrompt > 128) {
-    alert("Please choose a length of at least 8 characters and no more than 128 characters. Refresh the page and start over again.")
+    alert("Please choose a length of at least 8 characters and no more than 128 characters. Click the 'Generate Password' button to start over again.")
   } else {
     var password = generatePassword(passwordLengthPrompt, lowerCasePrompt, upperCasePrompt, numericPrompt, specialPrompt);
     var passwordText = document.querySelector("#password");
@@ -57,25 +76,3 @@ function charCodeArray(x, y) {
   }
   return array
 };
-
-// User prompts for password criteria. Create logic to only show next criteria if user inputs a valid option
-
-alert("Welcome to the Password Generator! You will be asked a series of questions to select your password criteria. Select 'Generate Password' when you have answered all questions.");
-
-var lowerCasePrompt = prompt("Do you want the password to include lowercase character\(s)\? Answer 'yes' or 'no.'");
-lowerCasePrompt = lowerCasePrompt.toLowerCase();
-
-
-var upperCasePrompt = prompt("Do you want the password to include uppercase character\(s)\? Answer 'yes' or 'no.'");
-upperCasePrompt = upperCasePrompt.toLowerCase();
-
-
-var numericPrompt = prompt("Do you want the password to include numeric character\(s)\? Answer 'yes' or 'no.'");
-numericPrompt = numericPrompt.toLowerCase();
-
-
-var specialPrompt = prompt("Do you want the password to include special character\(s)\? Answer 'yes' or 'no.'");
-specialPrompt = specialPrompt.toLowerCase();
-
-var passwordLengthPrompt = prompt("Please choose a password length of at least 8 characters and no more than 128 characters.");
-
